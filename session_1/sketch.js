23a81362aa814b5c702ed05969245c20087f29ef
Maija <hello@maijagrudule.com>
2019-05-16 21:26:36 +0200

    fly

--- session_1/sketch.js
+++ session_1/sketch.js
@@ -1,69 +1,70 @@
 var w;
-
+//array of walker objects
 let eyes = [];
 
 
 function setup() {
   // put setup code here
   createCanvas(640, 640);
-  for (let i = 0; i < 20; i++) {
-    eyes[i] = new Walker(width / 2, height / 2);
-  }
+  // make 20 eyes
+
+  w = new Walker(width / 2, height / 2, 255, 0);
+
 }
 
 function draw() {
   // put drawing code here
   background(51);
+  // draws 20 eyes
 
-
-
-  for (let i = 0; i < eyes.length; i++) {
-
-    eyes[i].display(255, 0);
-    eyes[i].update();
-  }
+  w.display();
+  w.update();
 
 
 }
 
-function Walker(x, y) {
+function Walker(x, y, eyeColor, irisColor) {
   var xoff = TWO_PI;
-  this.pos = createVector(x, y);
+  var xoff2 = TWO_PI;
+  this.pos = createVector(0, 0);
   this.posTrace = createVector(0, 0);
   this.vel = createVector(0, 0);
-  this.delay = createVector(0, 10);
-  this.acc = p5.Vector.fromAngle(random(TWO_PI));
-  this.acc.setMag(0.2);
+  this.velOrbit = createVector(100, 10);
+  this.acc = createVector(0, 0);
+  // this.acc = p5.Vector.fromAngle(random(TWO_PI));
+  this.acc.setMag(0.002);
 
 
-  this.display = function(color, trace) {
-    fill(color);
-    ellipse(this.pos.x, this.pos.y, 48, 48);
-    fill(trace);
-    ellipse(this.posTrace.x - 9, this.posTrace.y - 9, 18, 18);
+  this.display = function() {
+
+    fill(eyeColor);
+    ellipse(this.pos.x, this.pos.y, 38, 38);
+
+    //add the iris
+    fill(irisColor);
+
+    rect(this.posTrace.x, this.posTrace.y, 18, 9);
 
   }
 
   this.update = function() {
 
-
     var mouse = createVector(mouseX, mouseY);
+    // mouse.setMag(10);
+    this.pos = mouse;
 
-    mouse.setMag(18);
-    this.acc.rotate(noise(xoff) * 300);
 
-    xoff += 0.001;
     this.vel.add(this.acc);
     this.pos.add(this.vel);
+
+
+    this.velOrbit.rotate(noise(xoff) / 10);
+    xoff += 0.00030;
+    this.velOrbit.add(random(-10, 10));
+    xoff2 += 0.030;
     this.posTrace.set(this.pos);
-    this.posTrace.add(mouse);
     this.posTrace.add(this.acc);
-    this.posTrace.add(this.vel);
-
-    // console.log(this.posTrace)
-    // this.posTrace.setMag(1);
-
-
+    this.posTrace.add(this.velOrbit);
 
   }
 
